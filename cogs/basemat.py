7dff8549a12f70ff66fabcd793124a7294991c6a
VictorHS82 <hugoscatamberlo@gmail.com>
2026-07-29 19:38:00 -0300

    Mudanças no sistema de rolagem

--- cogs/basemat.py
+++ cogs/basemat.py
@@ -1,53 +1,33 @@
 from discord.ext import commands
 import random
+from game_logic.diceman import Diceman
+
 
 class Basemat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.diceman = Diceman(bot)
 
-    @commands.hybrid_command(name="d", description="Responde com o resultado")
-    async def d(self, ctx, lados: int):
-        resultado = random.randint(1, lados)
-        await ctx.reply(f"d{lados}: {resultado}") 
+    @commands.hybrid_command(name="roll", description="Role dados")
+    async def roll(self, ctx, expressao: str):
+        resposta = await self.diceman.roll_dice(expressao)
+        await ctx.reply(resposta)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        dados = []
-        total = 0
-        positive = True
         if message.author.bot:
             return
 
-        if message.content.startswith("!"):
-            comando = message.content[1:]
-            if "+" in comando:
-                dice, bonus = comando.split("+")
-                quantidade, lados = dice.split("d")
-            elif "-" in comando:
-                dice, bonus = comando.split("-")
-                quantidade, lados = dice.split("d")
-                positive = False
-            else:
-                quantidade, lados = comando.split("d")
-                bonus = 0
-            if quantidade == "":
-                quantidade = 1
+        if not message.content.startswith("!"):
+            return
 
-            lados = int(lados)
-            quantidade = int(quantidade)
-            bonus = int(bonus)
-            if positive == False:
-                bonus = bonus*(-1)
+        comando = message.content[1:].strip()
+        if not comando:
+            return
 
-            for i in range(quantidade):
-                resultado = random.randint(1, lados)
-                dados.append(resultado)
-                total += resultado
-            if bonus > 0:
-                await message.reply(f"{quantidade}d{lados}+{bonus}: {dados}+{bonus} = {total+bonus}") 
-            elif bonus == 0: 
-                await message.reply(f"{quantidade}d{lados}: {dados} = {total}")
-            elif bonus < 0:
-                await message.reply(f"{quantidade}d{lados}{bonus}: {dados}{bonus} = {total+bonus}")
+        resposta = await self.diceman.roll_dice(comando)
+        await message.reply(resposta)
+
+
 async def setup(bot):
     await bot.add_cog(Basemat(bot))
